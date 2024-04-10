d03dd2bbc3aa95d0d3f8dd064f08ee98e2a0dcdf
Jorge Benitez <a_jorge.benitez.llado@iespablopicasso.es>
2024-04-10 23:17:01 +0200

    Hago una calculadora basica

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,41 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenido a la Calculadora");
+
+        System.out.print("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Introduce la operación (+ para suma, - para resta): ");
+        char operacion = scanner.next().charAt(0);
+
+        if (operacion == '+') {
+            double resultado = suma(num1, num2);
+            System.out.println("El resultado de la suma es: " + resultado);
+        } else if (operacion == '-') {
+            double resultado = resta(num1, num2);
+            System.out.println("El resultado de la resta es: " + resultado);
+        } else {
+            System.out.println("Operación no válida.");
+        }
+
+        scanner.close();
+    }
+
+    public static double suma(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double resta(double num1, double num2) {
+        return num1 - num2;
+    }
+}
